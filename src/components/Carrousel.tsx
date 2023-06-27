af03090eaff032d5b6f37fecd6a1633a6bfb69ac
epalazon <epalazon@sistran.com.ar>
2023-06-27 13:04:43 -0300

    se agrego el carrousel sin terminar

--- src/components/Carrousel.tsx
+++ src/components/Carrousel.tsx
@@ -1,14 +1,14 @@
-import { Grid } from '@mui/material';
-interface Props {
-    images: string[];
-    autoPlay?: boolean;
-    showButtons?: boolean;
-  }
+// import { Grid } from '@mui/material';
+// interface Props {
+//     images: string[];
+//     autoPlay?: boolean;
+//     showButtons?: boolean;
+//   }
 
-export default function Carrousel(props: Props) {
-    return(
-        <Grid item xs={12}>
+// export default function Carrousel(props: Props) {
+//     return(
+//         <Grid item xs={12}>
 
-        </Grid>
-    )
-}
+//         </Grid>
+//     )
+// }
